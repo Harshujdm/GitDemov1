e58f47617b1a851f941937f54c14e0808092311a
M1064277 <M1064277@MT-PG02AL1S>
2021-06-08 15:14:16 +0530

    First Commit..

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+	public static void main(String[] args) {
+		String name ="Lamborgini";
+		
+		if(name == "Lamborgini")
+			System.out.println("This is a car manufacturing brand");
+		else {
+			System.out.println("Enter the correct car manufacturer");
+		}
+	}
+
+}
